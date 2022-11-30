5739ce1b441f1203f3cf4e26ca5c3cafeae04ae6
KULDEEP SINGH NEGI <nkuldeep615@gmail.com>
2022-11-30 18:37:52 +0530

    prime_number

---
+++ Palindrome.js
@@ -0,0 +1,10 @@
+let str="naman";
+let bag = "";
+    for(let i=0; i<=str.length ; i++){
+        bag=bag+str[i];
+    }
+    if(bag===str){
+        console.log("Yes");
+    }else{
+        console.log("No");
+    }
